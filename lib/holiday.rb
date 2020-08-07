13c30de0dda83ed99a6c997055aa471f4ae20c4d
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-07 12:48:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holiday|
     holiday.collect do |holiday, decorations|
-      if item.include?("BBQ")
+      if decorations.include?("BBQ")
         holiday 
       end
     end
