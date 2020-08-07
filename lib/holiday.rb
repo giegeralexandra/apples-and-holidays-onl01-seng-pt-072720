ed9ffc3450717a98830d74cd105da6e96c78debe
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-07 13:13:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,9 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season}".capitalize.to_s + ":"
     holiday.each do |holiday, decorations|
+      holiday.split.to_s 
       decorations.each do 
+        
         puts "  #{holiday.capitalize.to_s}" + ":" + " #{decorations.join(", ")}"
       end
     end
